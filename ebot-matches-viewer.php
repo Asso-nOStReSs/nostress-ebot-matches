3c27297d0decf82cb5f52ae64c1aeddd52dbb6e8
BRAMILLE Sébastien <sebastien.bramille@gmail.com>
2016-01-04 15:41:57 +0000

    FIX coding style

--- ebot-matches-viewer.php
+++ ebot-matches-viewer.php
@@ -1,22 +1,22 @@
 <!-- Ici Le CSS du tableau d'affichage -->
 <style>
-table.ebot tr:nth-child(odd){
-  background-color:#282828 ;
-}
-table.ebot {
-	border: 0pt;
-}
-th.ebot {
-	border: 1pt solid black;
-	-moz-border-radius:10px;
-	-webkit-border-radius:10px;
-	border-radius:10px;
-}
+    table.ebot tr:nth-child(odd){
+        background-color:#282828 ;
+    }
+    table.ebot {
+        border: 0pt;
+    }
+    th.ebot {
+        border: 1pt solid black;
+        -moz-border-radius:10px;
+        -webkit-border-radius:10px;
+        border-radius:10px;
+    }
 
-tr.border_bottom td {
-	border-bottom: 1pt solid black;
-	border-right: 0pt;
-}
+    tr.border_bottom td {
+        border-bottom: 1pt solid black;
+        border-right: 0pt;
+    }
 </style>
 <!-- FIN du CSS -->
 
@@ -34,255 +34,255 @@ Author URI: www.boudjelalyannick.fr
 add_action('widgets_init','emv_init');
 
 function emv_init(){
-	register_widget("emv_widget");
+    register_widget("emv_widget");
 }
 
 class emv_widget extends WP_widget{
-	function emv_widget() {
-		$options = array(
-			"classname"=>"ebot-matches",
-			"description"=>"Affiche les scrores, effectuer avec l'eBot sur vos serveurs."
-		);
-	/*	$control = array(
-			"width"=>1000,
-			"height"=>500
-		);
-	*/
-		$this->WP_widget("emv-ebot-matches","eBoT Matches Viewer",$options);
-	}
+    function emv_widget() {
+        $options = array(
+            "classname"=>"ebot-matches",
+            "description"=>"Affiche les scrores, effectuer avec l'eBot sur vos serveurs."
+        );
+        /*	$control = array(
+                "width"=>1000,
+                "height"=>500
+            );
+        */
+        $this->WP_widget("emv-ebot-matches","eBoT Matches Viewer",$options);
+    }
 
-	function widget($args,$d){
-		extract($args);
-		echo $before_widget;
-		echo $before_title.$d["titre"].$after_title;
+    function widget($args,$d){
+        extract($args);
+        echo $before_widget;
+        echo $before_title.$d["titre"].$after_title;
 
-/* OPTION A */
-			if ($d["typeconnect"] == "A"){
+        /* OPTION A */
+        if ($d["typeconnect"] == "A"){
 
-				/* Connection Distante mysql */
-				$web= $d["web"];
-				$host= $d["host"];
-				$dbnamedist= $d["dbnamedistant"];
-				$userdist= $d["userdistant"];
-				$passworddist= $d["passworddistant"];
-				$nbrmax= $d["nbrmax"];
+            /* Connection Distante mysql */
+            $web= $d["web"];
+            $host= $d["host"];
+            $dbnamedist= $d["dbnamedistant"];
+            $userdist= $d["userdistant"];
+            $passworddist= $d["passworddistant"];
+            $nbrmax= $d["nbrmax"];
 
-					try{
-					$bdd = new PDO('mysql:host='.$host.';dbname='.$dbnamedist.'', ''.$userdist.'', ''.$passworddist.'');
-					$affreq = $bdd->prepare('SELECT id, team_a_name, team_b_name, score_a, score_b FROM matchs ORDER BY id DESC LIMIT 0, '.$nbrmax.'');
-					$affreq->execute();
-					}
-					catch (Exception $e)
-					{
-							die('Erreur : ' . $e->getMessage());
-					}
+            try{
+                $bdd = new PDO('mysql:host='.$host.';dbname='.$dbnamedist.'', ''.$userdist.'', ''.$passworddist.'');
+                $affreq = $bdd->prepare('SELECT id, team_a_name, team_b_name, score_a, score_b FROM matchs ORDER BY id DESC LIMIT 0, '.$nbrmax.'');
+                $affreq->execute();
+            }
+            catch (Exception $e)
+            {
+                die('Erreur : ' . $e->getMessage());
+            }
 
-			}
+        }
 
-/* OPTION B */
-			if ($d["typeconnect"] == "B"){
-				echo "Option B en developement";
-			}
+        /* OPTION B */
+        if ($d["typeconnect"] == "B"){
+            echo "Option B en developement";
+        }
 
-/* OPTION C */
-			if ($d["typeconnect"] == "C"){
-				$dbnamelocal= $d["dbnamelocal"];
-				$userlocal= $d["userlocal"];
-				$passwordlocal= $d["passwordlocal"];
-				$nbrmax= $d["nbrmax"];
+        /* OPTION C */
+        if ($d["typeconnect"] == "C"){
+            $dbnamelocal= $d["dbnamelocal"];
+            $userlocal= $d["userlocal"];
+            $passwordlocal= $d["passwordlocal"];
+            $nbrmax= $d["nbrmax"];
 
-				try{
-					$bdd = new PDO('mysql:host=localhost;dbname='.$dbnamelocal.'', ''.$userlocal.'', ''.$passwordlocal.'');
-					$req = 'SELECT id, team_a_name, team_b_name, score_a, score_b FROM matchs ORDER BY id DESC LIMIT 0, '.$nbrmax.'';
-					echo 'Connecter à la base Mysql locale. <br />';
-					}
-					catch (Exception $e)
-					{
-							die('Erreur : ' . $e->getMessage());
-					}
+            try{
+                $bdd = new PDO('mysql:host=localhost;dbname='.$dbnamelocal.'', ''.$userlocal.'', ''.$passwordlocal.'');
+                $req = 'SELECT id, team_a_name, team_b_name, score_a, score_b FROM matchs ORDER BY id DESC LIMIT 0, '.$nbrmax.'';
+                echo 'Connecter à la base Mysql locale. <br />';
+            }
+            catch (Exception $e)
+            {
+                die('Erreur : ' . $e->getMessage());
+            }
 
-			}
+        }
 
-/* TABLEAU */		
-					echo'<table class="ebot">';
-					echo "<tr><th class='ebot'>#Id</th><th class='ebot'>Score</th></tr>";
-					
-					while($row = $affreq->fetch(PDO::FETCH_ASSOC)){
-						$team1name= $row['team_a_name'];
-						$team2name= $row['team_b_name'];
-						$team1scr= $row['score_a'];
-						$team2src= $row['score_b'];
+        /* TABLEAU */
+        echo'<table class="ebot">';
+        echo "<tr><th class='ebot'>#Id</th><th class='ebot'>Score</th></tr>";
 
-						echo "<tr class='border_bottom'><td>";
-						echo '<a href="http://'.$web.'/eBot-CSGO/matchs/view/'.$row['id'].'" target="_blank">'.$row['id'].'</a>';
-						echo '</td><td><a href="http://'.$web.'/eBot-CSGO/matchs/view/'.$row['id'].'" target="_blank">';
+        while($row = $affreq->fetch(PDO::FETCH_ASSOC)){
+            $team1name= $row['team_a_name'];
+            $team2name= $row['team_b_name'];
+            $team1scr= $row['score_a'];
+            $team2src= $row['score_b'];
 
-							if($team1scr>$team2src) 
-								echo '<strong>'.$team1name.'&nbsp;-&nbsp;<font color="green">'.$team1scr.'</strong></font>&nbsp;:&nbsp;<font color="red">'.$team2src.'</font>&nbsp;-&nbsp;'.$team2name.'';
-							elseif($team1scr<$team2src)
-								echo ''.$team1name.'&nbsp;-&nbsp;<font color="red">'.$team1scr.'</font>&nbsp;:&nbsp;<font color="green">'.$team2src.'</font>&nbsp;-&nbsp;<strong>'.$team2name.'</strong>';
-							else 
-								echo ''.$team1name.'&nbsp;-&nbsp;<font color="bleue">'.$team1scr.'</font>&nbsp;:&nbsp;<font color="bleue">'.$team2src.'</font>&nbsp;-&nbsp;'.$team2name.'';
-						echo "</a></td></tr>";
-					}
+            echo "<tr class='border_bottom'><td>";
+            echo '<a href="http://'.$web.'/eBot-CSGO/matchs/view/'.$row['id'].'" target="_blank">'.$row['id'].'</a>';
+            echo '</td><td><a href="http://'.$web.'/eBot-CSGO/matchs/view/'.$row['id'].'" target="_blank">';
 
-					echo'</table>';
-		echo $after_widget;
-		$bdd == null;
-	}
+            if($team1scr>$team2src)
+                echo '<strong>'.$team1name.'&nbsp;-&nbsp;<font color="green">'.$team1scr.'</strong></font>&nbsp;:&nbsp;<font color="red">'.$team2src.'</font>&nbsp;-&nbsp;'.$team2name.'';
+            elseif($team1scr<$team2src)
+                echo ''.$team1name.'&nbsp;-&nbsp;<font color="red">'.$team1scr.'</font>&nbsp;:&nbsp;<font color="green">'.$team2src.'</font>&nbsp;-&nbsp;<strong>'.$team2name.'</strong>';
+            else
+                echo ''.$team1name.'&nbsp;-&nbsp;<font color="bleue">'.$team1scr.'</font>&nbsp;:&nbsp;<font color="bleue">'.$team2src.'</font>&nbsp;-&nbsp;'.$team2name.'';
+            echo "</a></td></tr>";
+        }
 
-	function update($new,$old){
-		return $new;
-	}
+        echo'</table>';
+        echo $after_widget;
+        $bdd == null;
+    }
 
-	function form($d) {
-		$defaut = array(
-			"titre" => "eBoT Matches",
-			"nbrmax" => "5",
-			"userdistant" => "ebotv3",
-			"userlocal" => "ebotv3",
-			"dbnamedistant" => "ebotv3",
-			"dbnamelocal" => "ebotv3"
-		);
-		$d = wp_parse_args($d,$defaut)
-?>
+    function update($new,$old){
+        return $new;
+    }
 
-			<div id="form">
+    function form($d) {
+        $defaut = array(
+            "titre" => "eBoT Matches",
+            "nbrmax" => "5",
+            "userdistant" => "ebotv3",
+            "userlocal" => "ebotv3",
+            "dbnamedistant" => "ebotv3",
+            "dbnamelocal" => "ebotv3"
+        );
+        $d = wp_parse_args($d,$defaut)
+        ?>
 
-			<p>
+        <div id="form">
 
-				<label for="<?php echo $this->get_field_id("titre"); ?>">Titre : </label>
+            <p>
 
-				<input value="<?echo $d["titre"];?>" name="<?php echo $this->get_field_name("titre"); ?>" id="<?php echo $this->get_field_id("titre"); ?>" type="text"/>
+                <label for="<?php echo $this->get_field_id("titre"); ?>">Titre : </label>
 
-			</p>
+                <input value="<?echo $d["titre"];?>" name="<?php echo $this->get_field_name("titre"); ?>" id="<?php echo $this->get_field_id("titre"); ?>" type="text"/>
 
-			<p>
+            </p>
 
-				<label for="<?php echo $this->get_field_id("nbrmax"); ?>">Nombre max de match : </label>
+            <p>
 
-				<input value="<?echo $d["nbrmax"];?>" name="<?php echo $this->get_field_name("nbrmax"); ?>" id="<?php echo $this->get_field_id("nbrmax"); ?>" type="text" maxlength="1"/>
+                <label for="<?php echo $this->get_field_id("nbrmax"); ?>">Nombre max de match : </label>
 
-			</p>
+                <input value="<?echo $d["nbrmax"];?>" name="<?php echo $this->get_field_name("nbrmax"); ?>" id="<?php echo $this->get_field_id("nbrmax"); ?>" type="text" maxlength="1"/>
 
-			<p>
+            </p>
 
-				<label for="<?php echo $this->get_field_id("typeconnect"); ?>">Type de connection : </label>
+            <p>
 
-				<select value="<?echo $d["typeconnect"];?>" name="<?php echo $this->get_field_name("typeconnect");?>" id="<?php echo $this->get_field_id("typeconnect"); ?>">
+                <label for="<?php echo $this->get_field_id("typeconnect"); ?>">Type de connection : </label>
 
-					<option value="A">Distant (Option:A)</option>
+                <select value="<?echo $d["typeconnect"];?>" name="<?php echo $this->get_field_name("typeconnect");?>" id="<?php echo $this->get_field_id("typeconnect"); ?>">
 
-					<option value="B">Online (Option:B)</option>
+                    <option value="A">Distant (Option:A)</option>
 
-					<option value="C">Local (Option:C)</option>
+                    <option value="B">Online (Option:B)</option>
 
-				</select>
+                    <option value="C">Local (Option:C)</option>
 
-			</p>
+                </select>
 
-			</div>
+            </p>
 
-			<div id="A" class="divoption">
+        </div>
 
-				<p>Option A "Distant":</p>
+        <div id="A" class="divoption">
 
-					<p>
+            <p>Option A "Distant":</p>
 
-						<label for="<?php echo $this->get_field_id("web"); ?>">Website eBot : </label>
+            <p>
 
-						<input value="<?echo $d["web"];?>" name="<?php echo $this->get_field_name("web"); ?>" id="<?php echo $this->get_field_id("web"); ?>" type="text"/>/eBot-CSGO/
+                <label for="<?php echo $this->get_field_id("web"); ?>">Website eBot : </label>
 
-					</p>
-				
-					<p>
+                <input value="<?echo $d["web"];?>" name="<?php echo $this->get_field_name("web"); ?>" id="<?php echo $this->get_field_id("web"); ?>" type="text"/>/eBot-CSGO/
 
-						<label for="<?php echo $this->get_field_id("host"); ?>">Ip du Host : </label>
+            </p>
 
-						<input value="<?echo $d["host"];?>" name="<?php echo $this->get_field_name("host"); ?>" id="<?php echo $this->get_field_id("host"); ?>" type="text"/>
+            <p>
 
-					</p>
+                <label for="<?php echo $this->get_field_id("host"); ?>">Ip du Host : </label>
 
-					<p>
+                <input value="<?echo $d["host"];?>" name="<?php echo $this->get_field_name("host"); ?>" id="<?php echo $this->get_field_id("host"); ?>" type="text"/>
 
-						<label for="<?php echo $this->get_field_id("dbnamedistant"); ?>">Nom de la Base de donnée : </label>
+            </p>
 
-						<input value="<?echo $d["dbnamedistant"];?>" name="<?php echo $this->get_field_name("dbnamedistant"); ?>" id="<?php echo $this->get_field_id("dbnamedistant"); ?>" type="text"/>
+            <p>
 
-					</p>
+                <label for="<?php echo $this->get_field_id("dbnamedistant"); ?>">Nom de la Base de donnée : </label>
 
-					<p>
+                <input value="<?echo $d["dbnamedistant"];?>" name="<?php echo $this->get_field_name("dbnamedistant"); ?>" id="<?php echo $this->get_field_id("dbnamedistant"); ?>" type="text"/>
 
-						<label for="<?php echo $this->get_field_id("userdistant"); ?>">Utilisateur "login" : </label>
+            </p>
 
-						<input value="<?echo $d["userdistant"];?>" name="<?php echo $this->get_field_name("userdistant"); ?>" id="<?php echo $this->get_field_id("userdistant"); ?>" type="text"/>
+            <p>
 
-					</p>
+                <label for="<?php echo $this->get_field_id("userdistant"); ?>">Utilisateur "login" : </label>
 
-					<p>
+                <input value="<?echo $d["userdistant"];?>" name="<?php echo $this->get_field_name("userdistant"); ?>" id="<?php echo $this->get_field_id("userdistant"); ?>" type="text"/>
 
-						<label for="<?php echo $this->get_field_id("passworddistant"); ?>">Password : </label>
+            </p>
 
-						<input value="<?echo $d["passworddistant"];?>" name="<?php echo $this->get_field_name("passworddistant"); ?>" id="<?php echo $this->get_field_id("passworddistant"); ?>" type="text"/>
+            <p>
 
-					</p>
+                <label for="<?php echo $this->get_field_id("passworddistant"); ?>">Password : </label>
 
-			</div>
+                <input value="<?echo $d["passworddistant"];?>" name="<?php echo $this->get_field_name("passworddistant"); ?>" id="<?php echo $this->get_field_id("passworddistant"); ?>" type="text"/>
 
-			<hr>
+            </p>
 
-			<div id="B" class="divoption">
+        </div>
 
-				<p>Option B "Online": "Non fonctionnel"</p>
+        <hr>
 
-					<p> En developement !
+        <div id="B" class="divoption">
 
-						<!-- <label for="<?php echo $this->get_field_id("nom-team"); ?>">Nom Team à afficher : </label>
+            <p>Option B "Online": "Non fonctionnel"</p>
+
+            <p> En developement !
+
+                <!-- <label for="<?php echo $this->get_field_id("nom-team"); ?>">Nom Team à afficher : </label>
 
 						<input value="<?echo $d["nom-team"];?>" name="<?php echo $this->get_field_name("nom-team"); ?>" id="<?php echo $this->get_field_id("nom-team"); ?>" type="text"/> -->
 
-					</p>
+            </p>
 
-			</div>
+        </div>
 
-			<hr>
+        <hr>
 
-			<div id="C" class="divoption">
+        <div id="C" class="divoption">
 
-				<p>Option C "Local:</p>
+            <p>Option C "Local:</p>
 
-					<p>
+            <p>
 
-						<label for="<?php echo $this->get_field_id("dbnamelocal"); ?>">Nom de la Base de donnée : </label>
+                <label for="<?php echo $this->get_field_id("dbnamelocal"); ?>">Nom de la Base de donnée : </label>
 
-						<input value="<?echo $d["dbnamelocal"];?>" name="<?php echo $this->get_field_name("dbnamelocal"); ?>" id="<?php echo $this->get_field_id("dbnamelocal"); ?>" type="text"/>
+                <input value="<?echo $d["dbnamelocal"];?>" name="<?php echo $this->get_field_name("dbnamelocal"); ?>" id="<?php echo $this->get_field_id("dbnamelocal"); ?>" type="text"/>
 
-					</p>
+            </p>
 
-					<p>
+            <p>
 
-						<label for="<?php echo $this->get_field_id("userlocal"); ?>">Utilisateur "login" : </label>
+                <label for="<?php echo $this->get_field_id("userlocal"); ?>">Utilisateur "login" : </label>
 
-						<input value="<?echo $d["userlocal"];?>" name="<?php echo $this->get_field_name("userlocal"); ?>" id="<?php echo $this->get_field_id("userlocal"); ?>" type="text"/>
+                <input value="<?echo $d["userlocal"];?>" name="<?php echo $this->get_field_name("userlocal"); ?>" id="<?php echo $this->get_field_id("userlocal"); ?>" type="text"/>
 
-					</p>
+            </p>
 
-					<p>
+            <p>
 
-						<label for="<?php echo $this->get_field_id("passwordlocal"); ?>">Password : </label>
+                <label for="<?php echo $this->get_field_id("passwordlocal"); ?>">Password : </label>
 
-						<input value="<?echo $d["passwordlocal"];?>" name="<?php echo $this->get_field_name("passwordlocal"); ?>" id="<?php echo $this->get_field_id("passwordlocal"); ?>" type="text"/>
+                <input value="<?echo $d["passwordlocal"];?>" name="<?php echo $this->get_field_name("passwordlocal"); ?>" id="<?php echo $this->get_field_id("passwordlocal"); ?>" type="text"/>
 
-					</p>
+            </p>
 
-			</div>
+        </div>
 
-			<p>Merci DeStrO pour l'eBOt. Widget dev. par Bouman.</p>
+        <p>Merci DeStrO pour l'eBOt. Widget dev. par Bouman.</p>
 
-		<?
+        <?
 
-	}
+    }
 
-}	
+}
 
 ?>
